aba6d674bfac22a6dc42699f70c0d79e4b5ce8bd
Khesa <vandikalvandi@gmail.com>
2023-06-15 01:54:01 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
-import removePreloads from 'remove_preload';
+import removePreloads from './remove-preloads'
 
 
 export default defineConfig({
