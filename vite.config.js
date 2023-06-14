45e286205ba8916747b58f679b3138abd92ee8d3
Khesa <vandikalvandi@gmail.com>
2023-06-15 01:47:10 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
-
+import removePreloads from '@/remove_preload';
 
 
 export default defineConfig({
     plugins: [
+        removePreloads(),
         laravel({
             input: [
                 'resources/sass/app.scss',
