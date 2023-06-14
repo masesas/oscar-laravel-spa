c1c4061c6dc5cde4353918e2cc2c057a85795ab1
Khesa <vandikalvandi@gmail.com>
2023-06-14 20:41:34 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -13,6 +13,7 @@ export default defineConfig({
             ],
             refresh: true,
             publicDirectory: '/public/',
+            buildDirectory: '/build/'
         }),
         vue({
             template: {
