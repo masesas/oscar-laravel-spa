5047dad3a9df73964c6a8202b81dcf53475209f7
Khesa <vandikalvandi@gmail.com>
2023-06-15 02:56:53 +0700

    update

--- resources/js/app.js
+++ resources/js/app.js
@@ -29,7 +29,7 @@ createInertiaApp({
     resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
     setup({ el, App, props, plugin }) {
         const myApp = createApp({ render: () => h(App, props) });
-        const assets = process.env.NODE_ENV != 'production' ? '/img' : 'https://otomotives.com/oscar';
+        const assets = process.env.NODE_ENV != 'production' ? '/img' : 'https://otomotives.com/oscar/img';
 
         myApp.config.globalProperties.$route = route
         myApp.config.globalProperties.$baseAssets = assets;
