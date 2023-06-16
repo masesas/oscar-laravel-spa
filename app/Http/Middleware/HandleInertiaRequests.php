f1058c5637554f9d14e7e1daf6a8192951c718ef
Khesa <vandikalvandi@gmail.com>
2023-06-17 00:08:46 +0700

    update

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -39,11 +39,11 @@ class HandleInertiaRequests extends Middleware
             'auth' => [
                 'user' => $request->user(),
             ],
-            // 'ziggy' => function () use ($request) {
-            //     return array_merge((new Ziggy)->toArray(), [
-            //         'location' => $request->url(),
-            //     ]);
-            // },
+            'ziggy' => function () use ($request) {
+                return array_merge((new Ziggy)->toArray(), [
+                    'location' => $request->url(),
+                ]);
+            },
             'flash' => [
                 'message' => fn () => session('message'),
                 'type' => fn () => session('type'),
