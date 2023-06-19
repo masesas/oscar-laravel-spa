a57641e7f88e38d77eae896948d665761621dca3
Khesa <vandikalvandi@gmail.com>
2023-06-19 17:11:38 +0700

    update

--- app/Http/Middleware/MeasureExecutionTime.php
+++ app/Http/Middleware/MeasureExecutionTime.php
@@ -16,14 +16,10 @@ class MeasureExecutionTime
     public function handle(Request $request, Closure $next): Response
     {
         if ($request->isJson()) {
-            // Get the response
             $response = $next($request);
 
-            // Calculate execution time
             $executionTime = microtime(true) - LARAVEL_START;
 
-            // I assume you're using valid json in your responses
-            // Then I manipulate them below
             $content = json_decode($response->getContent(), true) + [
                 'execution_time' => $executionTime,
             ];
