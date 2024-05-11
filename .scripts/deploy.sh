a29152c49a7879b3c4a9ef14d29feb1f0745f491
Khesa <vandikalvandi@gmail.com>
2024-05-12 02:07:09 +0700

    fix

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -34,44 +34,6 @@ npm run build
 # Exit maintenance mode
 php8.1 artisan up
 
-echo "Deployment finished!"
-
-#!/bin/bash
-set -e
-
-echo "Deploying..."
-
-# Enter maintenance mode or return true
-# if already is in maintenance mode
-(php8.1 artisan down) || true
-
-# restore any changes
-git restore .
-
-# Pull the latest version of the app
-git pull
-
-# Install composer dependencies
-php8.1 /usr/local/bin/composer install -q --no-dev --no-interaction --prefer-dist --optimize-autoloader
-
-# Clear the old cache
-php8.1 artisan clear-compiled
-
-# Recreate cache
-php8.1 artisan optimize
-
-# Install npm dependencies
-npm install
-
-# Compile npm assets
-npm run build
-
-# Run database migrations
-# php8.1 artisan migrate --force
-
-# Exit maintenance mode
-php8.1 artisan up
-
 echo "Copy public files... to root folder"
 cp -r /var/www/otomotives.com/oscar/public/* /var/www/otomotives.com/oscar/
 
