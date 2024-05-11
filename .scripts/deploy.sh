2c6679607011e97685675436b05b68d81f31d0a1
masesas <vandikalvandi@gmail.com>
2024-05-11 17:30:04 +0700

    installed octane via server and ignore ci/cd

---
+++ .scripts/deploy.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+set -e
+
+echo "Deploying..."
+
+# Enter maintenance mode or return true
+# if already is in maintenance mode
+(php8.1 artisan down) || true
+
+# restore any changes
+git restore .
+
+# Pull the latest version of the app
+git pull
+
+# Install composer dependencies
+php8.1 /usr/local/bin/composer install -q --no-dev --no-interaction --prefer-dist --optimize-autoloader
+
+# Clear the old cache
+php8.1 artisan clear-compiled
+
+# Recreate cache
+php8.1 artisan optimize
+
+# Install npm dependencies
+npm install
+
+# Compile npm assets
+npm run build
+
+# Run database migrations
+# php8.1 artisan migrate --force
+
+# Exit maintenance mode
+php8.1 artisan up
+
+echo "Deployment finished!"
